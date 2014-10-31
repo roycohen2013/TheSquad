cc67fa1be28a7e6d5c5ff94b07e9838d1376ad5d
roycohen2013 <roycohen122@gmail.com>
2014-10-31 17:27:12 -0400

    initial addition of ActionManager.py

---
+++ toolCloudProject/toolCloudApp/stateMachines/actionManager.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+state = 0
+
+if state == "userBorrowRequest":
+    pass
+elif state == "askOwner":
+    pass
+elif state == "acceptDecline":
+    pass
+elif state == "pickup":
+    pass
+elif state == "askOwner":
+    pass
