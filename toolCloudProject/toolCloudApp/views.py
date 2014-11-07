23c6c03fc45d85f30d99bb55ebf4a01512a83461
Garrala <caffinefree19@yahoo.com>
2014-11-07 17:45:55 -0500

    Fixed our User, Tool, Shed validation

--- toolCloudProject/toolCloudApp/views.py
+++ toolCloudProject/toolCloudApp/views.py
@@ -31,7 +31,7 @@ def user_register(request):
     if request.user.is_anonymous():
         if request.method == 'POST':
             form = UserRegistrationForm(request.POST)
-            if form.is_valid==True:
+            if form.is_valid:
                 form.save()
                 user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
                 if user is not None:
@@ -60,7 +60,7 @@ def tool_submission(request):
     else:
         if request.method == 'POST':
             form = ToolCreationForm(request.user, request.POST)
-            if form.is_valid==True:
+            if form.is_valid:
                 tool = form.save()
                 tool.save()
 
@@ -254,7 +254,7 @@ def create_tool_shed(request):
 	else:
 		if request.method == 'POST':
 			form = ShedCreationForm(request.user, request.POST)
-			if form.is_valid==True:
+			if form.is_valid:
 				shed = form.save()
                 #send email
                 #sendMail(request.user.email, "Your Tool Submission Has Been Accepted! ", "Hey there " + request.first_name + ", \n\nThanks for submitting your " + form.cleaned_data['name'] + " to ToolCloud.  We'll let you know when someone wants to borrow it. \n\nCheers, \n\nThe Squad")
