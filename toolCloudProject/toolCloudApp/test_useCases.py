d0386d33605a22a77ced795d72c78cb49635aeb0
Taikhoom Attar <tfa2773@rit.edu>
2014-11-07 13:55:43 -0500

    Useless tests initial commit, plus son dump of a default db after
    newSlate is run

---
+++ toolCloudProject/toolCloudApp/test_useCases.py
@@ -0,0 +1,51 @@
+"""
+These tests depend on Selenium and also expect you to have Firefox installed.
+"""
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.by import bytearray
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+from toolCloudApp.models import Profile, Tool, Shed
+import utilities.toolUtilities as toolUtils
+import utilities.profileUtilities as profUtils
+import utilities.shedUtilities as shedUtils
+
+class useCaseTests:
+	def testUC1 (self):
+		with open("populationControl.py") as f:
+			code = compile(f.read(), "populationControl", 'exec')
+			exec(code)
+		driver = webdriver.Firefox ()
+		driver.get ("http://localhost:8000")
+		
+		driver.find_element_by_id ("registerBtn").click() #Click the register button
+		
+		driver.find_element_by_id ("id_first_name").send_keys("Boromir") #Enter the name Boromir into the first name field
+		driver.find_element_by_id ("id_last_name").send_keys("so Denethor") #enter the name so Denethor into the last name field
+		
+		driver.find_element_by_id ("id_username").send_keys("borofgondor") #enter a username into the field
+		
+		driver.find_element_by_id ("id_email").send_keys("captain@minastirith.gov") #enter an email
+		
+		driver.find_element_by_id ("id_password1").send_keys ("faramir") #enter password
+		
+		driver.find_element_by_id ("id_password2").send_keys ("faramir") #reenter password
+		
+		driver.find_element_by_id ("id_phone_number").send_keys ("0000000000")
+		
+		driver.find_element_by_id ("id_share_zone").send_keys ("Minas Tirith")
+		
+		driver.findElement(By.xpath("//*[@value='Sign Up']")).click();
+		
+		Profile.objects.get (username = "borofgondor")
+	
+	def testUC2 (self):
+		with open("populationControl.py") as f:
+			code = compile(f.read(), "populationControl", 'exec')
+			exec(code)
+		
