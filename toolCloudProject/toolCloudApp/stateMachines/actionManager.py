ec15dd841452072acc7c46f0ec7fdb5fe8daa3d0
roycohen2013 <roycohen122@gmail.com>
2014-11-09 14:37:28 -0500

    added all action processing functionality

--- toolCloudProject/toolCloudApp/stateMachines/actionManager.py
+++ toolCloudProject/toolCloudApp/stateMachines/actionManager.py
@@ -1,89 +1,99 @@
 
+import sys
+sys.path.append("..")
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toolCloudProject.settings")
+from django.contrib.auth.models import User
+from django.utils import timezone
+from toolCloudApp.models import Profile, Tool, Shed, Notification, Action
+
+
+#reference
+    """
+    class Action(models.Model):
+        tool = models.ForeignKey('Tool', related_name='toolActions')#if tool, send to owner of tool
+        shed = models.ForeignKey('Shed', related_name='shedActions')#if shed, send to all admins of shed
+        admin = models.ForeignKey('Profile', related_name='adminActions')#returns list of actions that a user is controlling of
+        requester = models.ForeignKey('Profile', related_name='requesterActions')
+
+        actionType = models.CharField(max_length=20)#either tool, or shed
+        currrentState = models.CharField(max_length=20)
+        timestamps = models.CharField(max_length=560)#CSV timestamps for every state
+        workSpace = models.CharField(max_length=200)#for use in state machine
+    """
 
 
 
 
 
 
+#Re-process all action objects on every call
+for actionInstance in getAllActions():
+
+
+    #states allow system to process and respond to all actions asyncrounusly
+
+    #Tool borrow state machine
+    if isToolRequest() == True:
+        if state == "userBorrowRequest":
+            #procede to next one
+            pass
+        elif state == "askOwner":
+            #generate question string asking [owner] if [borrower]
+
+            #procede to next state
+            pass
+        elif state == "acceptDecline":
+            #get notification assosiated with object
+                #if (notification responded == true):
+                    #start timer for when tool is overdue (set end time)
+                    #move tool location to requesters shed
+                    #Continue to Borrowed state
+
+
+                #if notification responded fales - notify of denial and delete request
+
+            pass
+        elif state == "borrowed":
+            #check if borrowed is past timestamp
+                #Notify {requester} that they are overdraft and they should return [tool]
+                #Set canBorrow state to false
+                #move to overdraft state
+
+            #if (tool.isAvailable() == true):   #means tool has been returned
+                #move state to returned 
+
+            pass
+        elif state == "overDraft":
+            #if (tool.isAvailable() == true):   #means tool has been returned
+                #calculate how many days the tool is overdue and reduce user reputation until then
+                #set user.canBorrow state to true.
+                #notify requester thankyou for returning the tool finally!
+                #move state to returned 
+
+            pass
+        elif state == "returned":
+            #delete action object
+            #Notify tool owner that his tool has been returned
+            pass
 
 
 
-# load action object
+    #shed request state machine
 
-"""
-class Action(models.Model):
-	tool = models.ForeignKey('Tool', related_name='toolActions')#if tool, send to owner of tool
-	shed = models.ForeignKey('Shed', related_name='shedActions')#if shed, send to all admins of shed
-	admin = models.ForeignKey('Profile', related_name='adminActions')#returns list of actions that a user is controlling of
-	requester = models.ForeignKey('Profile', related_name='requesterActions')
+    if isShedRequest() == True:
+        if state == "userShedRequest":
+            #procede to next state
+            pass
+        elif state == "askAdmins":
+            #loop through all admins of shed
+                #generate question string asking [Admin] if [borrower]
 
-	actionType = models.CharField(max_length=20)#either tool, or shed
-	currrentState = models.CharField(max_length=20)
-	timestamps = models.CharField(max_length=560)#CSV timestamps for every state
-	workSpace = models.CharField(max_length=200)#for use in state machine
-"""
+                #proced to next state
+            pass
 
-#Tool borrow state machine
+        elif state == "acceptDecline":
+            #get all notificaitons assosiated with this action
+                #check if notification has been responded to correctly
 
-if state ==	"userBorrowRequest":
-	#procede to next one
-    pass
-elif state == "askOwner":
-	#generate question string asking [owner] if [borrower]
-
-	#procede to next state
-    pass
-elif state == "acceptDecline":
-	#get notification assosiated with object
-		#if (notification responded == true):
-			#start timer for when tool is overdue (set end time)
-			#move tool location to requesters shed
-			#Continue to Borrowed state
-
-
-		#if notification responded fales - notify of denial and delete request
-
-    pass
-elif state == "borrowed":
-	#check if borrowed is past timestamp
-		#Notify {requester} that they are overdraft and they should return [tool]
-		#Set canBorrow state to false
-		#move to overdraft state
-
-	#if (tool.isAvailable() == true): 	#means tool has been returned
-		#move state to returned 
-
-    pass
-elif state == "overDraft":
-	#if (tool.isAvailable() == true): 	#means tool has been returned
-		#calculate how many days the tool is overdue and reduce user reputation until then
-		#set user.canBorrow state to true.
-		#notify requester thankyou for returning the tool finally!
-		#move state to returned 
-
-    pass
-elif state == "returned":
-	#delete action object
-	#Notify tool owner that his tool has been returned
-    pass
-
-
-
-#shed request state machine
-
-
-if state ==	"userShedRequest":
-	#procede to next state
-    pass
-elif state == "askAdmins":
-	#loop through all admins of shed
-		#generate question string asking [Admin] if [borrower]
-
-		#proced to next state
-    pass
-
-elif state == "acceptDecline":
-	#get all notificaitons assosiated with this action
-		#check if notification has been responded to correctly
-
-    pass
+            pass
