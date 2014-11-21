55ea5a1e77f232bae7294084929b2f475a947c2f
Jake Dulin <jad5366@rit.edu>
2014-11-20 19:08:52 -0500

    Starting work on actionManager.py

--- toolCloudProject/toolCloudApp/stateMachines/actionManager.py
+++ toolCloudProject/toolCloudApp/stateMachines/actionManager.py
@@ -1,3 +1,21 @@
+"""
+What calls action manager:
+    automated
+    update to one of the action objects
+    call back from a notification object
+
+    For reference:
+
+    class Action(models.Model):
+        tool = models.ForeignKey('Tool', related_name='toolActions',null = True) #if tool, send to owner of tool
+        shed = models.ForeignKey('Shed', related_name='shedActions',null = True) #if shed, send to all admins of shed
+        requester = models.ForeignKey('Profile', related_name='requesterActions')
+
+        actionType = models.CharField(max_length=20)#either tool, or shed
+        currrentState = models.CharField(max_length=20)
+        timeStamps = models.CharField(max_length=560,default = "") #CSV timestamps for every state
+        workSpace = models.CharField(max_length=200,null = True) #for use in state machine
+"""
 
 import sys
 sys.path.append("..")
@@ -12,107 +30,75 @@ import utilities.toolUtilities as toolUtil
 import utilities.notificationUtilities as notifUtil
 import utilities.actionUtilities as actionUtil
 
-
-
-#what calls action manager:
-    #Automated
-    #update to one of the action objects
-    #call back from a notification object
-
-
-#reference
-    """
-    class Action(models.Model):
-        tool = models.ForeignKey('Tool', related_name='toolActions')#if tool, send to owner of tool
-        shed = models.ForeignKey('Shed', related_name='shedActions')#if shed, send to all admins of shed
-        admin = models.ForeignKey('Profile', related_name='adminActions')#returns list of actions that a user is controlling of
-        requester = models.ForeignKey('Profile', related_name='requesterActions')
-
-        actionType = models.CharField(max_length=20)#either tool, or shed
-        currrentState = models.CharField(max_length=20)
-        timestamps = models.CharField(max_length=560)#CSV timestamps for every state
-        workSpace = models.CharField(max_length=200)#for use in state machine
-    """
-
-
 def ProcessActions():
 
     #Re-process all action objects on every call
     for actionInstance in getAllActions():
 
-        #states allow system to process and respond to all actions asyncrounusly
+        #states allow system to process and respond to all actions asynchronously
 
         #Tool borrow state machine
-        if isToolRequest() == True: 
-            if actionInstance.currrentState == "userBorrowRequest":                                        #entry point
-                
+        if isToolRequest() == True:
 
-                #procede to next state
+            if actionInstance.currrentState == "userBorrowRequest":  #entry point
+                #proceed to next state where the owner is asked if this user can borrow his tool
                 actionInstance.currrentState = "askOwner"
                 actionInstance.save()
-                #ProcessActions()       #re-invoke entire state machine
-                
+                #ProcessActions()   #re-invoke entire state machine
 
             if actionInstance.currrentState == "askOwner":
-                #generate question string asking [owner] if [borrower]
+                #send a response notification to the user who's tool is being requested
+                #the response options will be "Accept" or "Decline"
                 
-                question = "can " + actionInstance.requester.name + " borrow the " actionInstance.tool.name + "from " + actionInstance.tool.shed
-                userOptions = ",Accept,Deny"                                                                  #adding questions           
+                question = "Can " + actionInstance.requester.name + " borrow your " + \
+                                actionInstance.tool.name + " from " + actionInstance.tool.shed + "?"
+                userOptions = "Accept,Deny" #adding options       
                 
-                Notification.createResponseNotif(actionInstance,actionInstance.tool.owner,question,options=options,userOptions):
+                notifUtil.createResponseNotif(actionInstance, actionInstance.tool.owner, \
+                                                            question, options = userOptions)
 
-
-                #procede to next state
+                #proceed to next state
                 actionInstance.currrentState = "acceptDecline"
                 actionInstance.save()
+
                 #ProcessActions()       #re-invoke entire state machine
 
 
             elif actionInstance.currrentState == "acceptDecline":
-
-
-                                                                                 #get notification assosiated with object
-                if notifHasResponse(getNotifOfAction(actionInstance)) == True:   #if (notification responded == true):
-                
+                #if the owner of the tool has responded to the tool request notification
+                if notifHasResponse(getNotifOfAction(actionInstance)) == True:
                    
-                    if notifUtil.getNotifResponse(getNotifOfAction(actionInstance)) == True:
-                        #start timer for when tool is overdue (set end time)\
-                        #move tool location to requesters shed
+                    if notifUtil.getNotifResponse(getNotifOfAction(actionInstance)) == 'Accept':
+                        #start timer for when tool is overdue (set end time)
+                        #move tool location to requester's shed
 
+                        #target shed is the requester's personal shed
                         targetShed = shedUtil.getShedByName(profileUtil.getUserOfProfile(actionInstance.requester).username + "'s Shed")
+                        #remove the tool from it's old location first
                         shedUtil.removeToolFromShed(actionInstance.tool.shed,actionInstance.tool)
+                        #then add the tool to the requester's personal shed
                         shedUtil.addToolToShed(targetShed,actionInstance.tool)
-
-
-                        #set borrower field to tool
+                        #update tool's borrower field
                         toolUtil.updateToolBorrower(actionInstance.tool,getUserOfProfile(actionInstance.requester))
 
-                        #procede to next state
+                        #proceed to next state
                         actionInstance.currrentState = "borrowed"
                         actionInstance.save()
                         actionUtil.getNotifOfAction(actionInstance).delete()
                         #ProcessActions()       #re-invoke entire state machine
 
+                    else: # the owner of the tool declined the borrowing of the tool
 
-                    else:
-                        #Verbally reject requester 
+                        #send an info notification to the requester saying he was denied
+                        response = "You have been swiftly rejected from borrowing " + \
+                                        actionInstance.tool.name + " from " + actionInstance.tool.shed
 
-                        response = "You " + actionInstance.requester.name + " have been swiftly rejected from borrowing " actionInstance.tool.name + " from " + actionInstance.tool.shed
-                        createInfoNotif(actionInstance,actionInstance.requester,response):
+                        notifUtil.createInfoNotif(actionInstance,actionInstance.requester,response):
 
-                        #procede to next state
+                        #proceed to next state
                         actionInstance.currrentState = "idle"
                         actionInstance.save()
                         #ProcessActions()       #re-invoke entire state machine
-
-
-
-                    
-
-
-                #ownerResponse = Action.getAllActionNotifications(actionInstance)
-
-                    #if notification responded fales - notify of denial and delete request
 
             elif actionInstance.currrentState == "borrowed":
                 #check if borrowed is past timestamp
@@ -123,7 +109,7 @@ def ProcessActions():
                 #if (tool.isAvailable() == true):   #means tool has been returned
                     #move state to returned 
 
-                #procede to next state
+                #proceed to next state
                 #actionInstance.currrentState = "borrowed"
                 #actionInstance.save()
                 #ProcessActions()       #re-invoke entire state machine
@@ -152,7 +138,7 @@ def ProcessActions():
 
         # if isShedRequest() == True:
         #     if state == "userShedRequest":
-        #         #procede to next state
+        #         #proceed to next state
         #         pass
         #     elif state == "askAdmins":
         #         #loop through all admins of shed
